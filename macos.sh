c25a427bad40f75f9751af4f29593d55fde61bb4
Ricardo Vega - ricveal <ricveal@gmail.com>
2022-02-27 11:27:07 +0100

    fix: add waiting after ssh ke generation

--- macos.sh
+++ macos.sh
@@ -33,6 +33,7 @@ echo "Generating an RSA token for GitHub"
 mkdir -p ~/.ssh
 touch ~/.ssh/config
 ssh-keygen -t rsa -b 4096
+read wait
 echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_rsa" | tee ~/.ssh/config
 eval "$(ssh-agent -s)"
 echo "run 'pbcopy < ~/.ssh/id_rsa.pub' and paste that into GitHub. Then press <ENTER>"
