8a3f50ab0d02c193e89d3bc7915b34b2ce81e175
Ricardo Vega <ricveal@gmail.com>
2025-12-15 09:14:58 +0100

    fix: add theme to nvim

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,20 @@
-/home/ricveal/.config/omarchy/current/theme/neovim.lua
+local theme = "tokyonight-night"
+-- local theme = "night-owl"
+
+return {
+  {
+    "haishanh/night-owl.vim",
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = theme,
+    },
+  },
+}
